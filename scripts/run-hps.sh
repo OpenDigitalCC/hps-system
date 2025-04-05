f155a1ed9e7e2f7a1532bd9219a5736b25d4ebd0
SJM <sjm@opusvl.com>
2025-04-05 12:35:01 +0200

    Updated to dynamic paths

--- scripts/run-hps.sh
+++ scripts/run-hps.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-HPS_CONFIG=/srv/hps-config/hps.conf
+SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+HPS_CONFIG="${SCRIPT_DIR}/../../hps-config/hps.conf"
+#HPS_CONFIG=/srv/hps-config/hps.conf
 
 while [[ ! -r "$HPS_CONFIG" ]]; do
-    echo "$0 Warning: File '$HPS_CONFIG' is not readable."
-    echo "$0 Initialising config"
-    /srv/hps/scripts/hps-initialise.sh
-    sleep 5
+  echo "$0 Warning: File '$HPS_CONFIG' is not readable."
+  echo "$0 Initialising config"
+  "$SCRIPT_DIR/hps-initialise.sh"
+#  /srv/hps/scripts/hps-initialise.sh
+  sleep 5
 done
 
 echo "[✓] File '$HPS_CONFIG' is available. Continuing..."
@@ -32,7 +35,7 @@ if cluster_file=$(get_active_cluster_filename 2>/dev/null)
   echo "[INFO] No active cluster configured — continuing without starting services"
   echo "[INFO] Services will be started once the cluster is configured."
   echo ""
-  echo "[INFO] run /srv/hps/cli/cluster-configure.sh"
+  echo "[INFO] run /srv/hps-system/cli/cluster-configure.sh"
 fi
 
 echo "[INFO] Handing over to supervisord"
