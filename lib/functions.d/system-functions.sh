927cfb68b2dd073ea42a16489b9d59e91d16231a
SJM <sjm@opusvl.com>
2025-05-13 10:48:26 +0200

    Add system utility functions to system-functions.sh
    This file contains various utility functions for system operations.

---
+++ lib/functions.d/system-functions.sh
@@ -0,0 +1,43 @@
+__guard_source || return
+# Define your functions below
+
+
+hps_services_start() {
+  configure_supervisor_services
+  reload_supervisor_config
+  supervisorctl -c "${HPS_SERVICE_CONFIG_DIR}/supervisord.conf" start all
+}
+
+hps_services_stop() {
+  supervisorctl -c "${HPS_SERVICE_CONFIG_DIR}/supervisord.conf" stop all
+}
+
+hps_services_restart() {
+  configure_supervisor_services
+  reload_supervisor_config
+  supervisorctl -c "${HPS_SERVICE_CONFIG_DIR}/supervisord.conf" restart all
+}
+
+
+export_dynamic_paths() {
+  local cluster_name="${1:-}"
+  local base_dir="${HPS_CLUSTER_CONFIG_BASE_DIR:-/srv/hps-config/clusters}"
+  local active_link="${base_dir}/active-cluster"
+
+  if [[ -z "$cluster_name" ]]; then
+    [[ -L "$active_link" ]] || {
+      echo "[✗] No active cluster and none specified." >&2
+      return 1
+    }
+    cluster_name=$(basename "$(readlink -f "$active_link")" .cluster)
+  fi
+
+  export CLUSTER_NAME="$cluster_name"
+  export HPS_CLUSTER_CONFIG_DIR="${base_dir}/${CLUSTER_NAME}"
+  export HPS_HOST_CONFIG_DIR="${HPS_CLUSTER_CONFIG_DIR}/hosts"
+
+  return 0
+}
+
+
+
