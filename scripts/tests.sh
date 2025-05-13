194b11771a3de6998f1abaadf4f22f1c92b4b055
SJM <sjm@opusvl.com>
2025-05-13 10:43:56 +0200

    Add test script for automated testing procedures
    This script contains tests to ensure code functionality and stability.

---
+++ scripts/tests.sh
@@ -0,0 +1,9 @@
+
+
+# make functions for tests of key operations
+# such as cgi-param get/set etc, and cliuster conf, hostconf, restarts etc
+# also test cgi functions as follows
+env QUERY_STRING="cmd=get_config&mac=525400123456" bash -x /srv/hps-system/http/cgi-bin/boot_manager.sh
+
+# then run test suite from here
+
