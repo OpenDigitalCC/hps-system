7b18d1befe7cb64b437c723394f196c48bd148b7
SJM <sjm@opusvl.com>
2025-05-13 17:27:07 +0200

    ``` Refactor kickstart scripts for dynamic type handling
    - Updated kickstart.sh to include type parameter.
    - Modified generate_ks function to handle different types.
    - Split kickstart configurations into type-specific functions.
    ```

--- http/cgi-bin/kickstart.sh
+++ http/cgi-bin/kickstart.sh
@@ -6,9 +6,10 @@ source "$(dirname "${BASH_SOURCE[0]}")/../../lib/functions.sh"
 mac=$(get_param "mac")
 [[ -z "$mac" ]] && { cgi_fail "Missing MAC"; exit 1; }
 
-# Optionally normalize
-mac=$(normalise_mac "$mac") || { cgi_fail "Invalid MAC"; exit 1; }
+type=$(get_param "type")
+[[ -z "$type" ]] && { cgi_fail "Missing type"; exit 1; }
+
 
 cgi_header_plain
-generate_ks_tch "$mac"
+generate_ks "$mac" "$type"
 
--- lib/functions.d/kickstart-functions.sh
+++ lib/functions.d/kickstart-functions.sh
@@ -1,34 +1,29 @@
 __guard_source || return
 # Define your functions below
 
-
-generate_ks_tch() {
+generate_ks() {
   local macid="$1"
+  local type="$2"
 
   host_config "$macid" || {
     echo "[✗] Failed to load host config for $macid" >&2
     return 1
   }
+hps_log info "[$macid]" "Requesting kickstart for $type"
 
-  cat <<EOF
-#version=RHEL9
-install
-lang en_US.UTF-8
-keyboard us
-timezone UTC --utc
-network --bootproto=static --ip=${HOST_IP} --netmask=${HOST_NETMASK} --gateway=${HOST_GATEWAY} --nameserver=${HOST_DNS} --hostname=${HOST_NAME} --device=eth0 --activate
-rootpw --plaintext changeme
-firewall --disabled
-authconfig --enableshadow --passalgo=sha512
+ks_common
+ks_host_${type}
+
+}
+
+ks_type_TCH () {
+cat <<EOF
 
 # iSCSI boot disk
 iscsi --target=${ISCSI_TARGET_IP}:3260 --portal=${ISCSI_IQN}
 ignoredisk --only-use=sda
 clearpart --all --initlabel
 part / --fstype=ext4 --size=10240
-
-bootloader --location=mbr
-reboot
 
 %packages
 @^minimal-environment
@@ -40,14 +35,58 @@ virt-manager
 openssh-server
 %end
 
+EOF
+}
+
+ks_type_SCH () {
+cat <<EOF
+
+# iSCSI boot disk
+iscsi --target=${ISCSI_TARGET_IP}:3260 --portal=${ISCSI_IQN}
+ignoredisk --only-use=sda
+clearpart --all --initlabel
+part / --fstype=ext4 --size=10240
+
+%packages
+@^minimal-environment
+kvm
+qemu-kvm
+libvirt
+virt-install
+virt-manager
+openssh-server
+%end
+
+EOF
+}
+
+ks_common () {
+  cat <<EOF
+#version=RHEL9
+install
+lang en_US.UTF-8
+keyboard us
+timezone UTC --utc
+network --bootproto=static --ip=${HOST_IP} --netmask=${HOST_NETMASK} --gateway=${HOST_GATEWAY} --nameserver=${HOST_DNS} --hostname=${HOST_NAME} --device=eth0 --activate
+rootpw --plaintext changeme
+firewall --disabled
+authconfig --enableshadow --passalgo=sha512
+
+bootloader --location=mbr
+reboot
+
 %post --log=/root/ks-post.log
 echo "Hardening TCH install..."
 sed -i 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
 systemctl enable sshd
 systemctl disable firewalld NetworkManager
 %end
+
 EOF
+
 }
+
+
 
 
 
