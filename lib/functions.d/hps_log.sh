12c0565f87dfa25a53667c0899126dfedc32b4e3
SJM <sjm@opusvl.com>
2025-05-27 16:38:36 +0200

    ** Commit Message: ** Rename and enhance `hps_log` function
    ** Body:**
    - Renamed `hps_log` to `hps_log-X` to avoid conflicts.
    - Added URL decoding capability to log messages.
    - Updated logging format to include function name in logs.

--- lib/functions.d/hps_log.sh
+++ lib/functions.d/hps_log.sh
@@ -1,6 +1,6 @@
 __guard_source || return
 
-hps_log() {
+hps_log-X() {
   local level="$1"; shift
   local msg="$*"
   local ident="${HPS_LOG_IDENT:-hps}"
@@ -13,7 +13,38 @@ hps_log() {
 
   # Attempt to log to file
   if [[ -w "$logfile" || ( ! -e "$logfile" && -w "$(dirname "$logfile")" ) ]]; then
-    echo "[${ts}] [$ident] [$level] $msg" >> "$logfile"
+    echo "[${ts}] [$ident] [$level] [${FUNCNAME[1]}] $msg" >> "$logfile"
+  else
+    logger -t "$ident" -p "user.err" "Failed to write to $logfile"
+  fi
+}
+
+
+
+hps_log() {
+  local level="$1"; shift
+  local raw_msg="$*"
+  local ident="${HPS_LOG_IDENT:-hps}"
+  local logfile="${HPS_LOG_DIR:-/var/log}/hps-system.log"
+  local ts
+  ts=$(date '+%Y-%m-%d %H:%M:%S')
+
+  # URL decode function
+  url_decode() {
+    local data="${1//+/ }"
+    printf '%b' "${data//%/\\x}"
+  }
+
+  # Decode the message
+  local msg
+  msg="$(url_decode "$raw_msg")"
+
+  # Send to syslog
+  logger -t "$ident" -p "user.${level,,}" "$msg"
+
+  # Write to file if possible
+  if [[ -w "$logfile" || ( ! -e "$logfile" && -w "$(dirname "$logfile")" ) ]]; then
+    echo "[${ts}] [$ident] [$level] [${FUNCNAME[1]}] $msg" >> "$logfile"
   else
     logger -t "$ident" -p "user.err" "Failed to write to $logfile"
   fi
