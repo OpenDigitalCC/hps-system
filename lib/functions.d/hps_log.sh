9acc1e060123e21effffe81713d0a3706ee345d5
SJM <sjm@opusvl.com>
2025-05-28 18:57:54 +0200

    Remove old hps_log-X function from hps_log.sh

--- lib/functions.d/hps_log.sh
+++ lib/functions.d/hps_log.sh
@@ -1,24 +1,5 @@
 __guard_source || return
 
-hps_log-X() {
-  local level="$1"; shift
-  local msg="$*"
-  local ident="${HPS_LOG_IDENT:-hps}"
-  local logfile="${HPS_LOG_DIR:-/var/log}/hps-system.log"
-  local ts
-  ts=$(date '+%Y-%m-%d %H:%M:%S')
-
-  # Send to syslog
-  logger -t "$ident" -p "user.${level,,}" "$msg"
-
-  # Attempt to log to file
-  if [[ -w "$logfile" || ( ! -e "$logfile" && -w "$(dirname "$logfile")" ) ]]; then
-    echo "[${ts}] [$ident] [$level] [${FUNCNAME[1]}] $msg" >> "$logfile"
-  else
-    logger -t "$ident" -p "user.err" "Failed to write to $logfile"
-  fi
-}
-
 
 
 hps_log() {
