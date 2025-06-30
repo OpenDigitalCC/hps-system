662ca10d436b412aef94cbb97738b06d9c3690c7
SJM <sjm@opusvl.com>
2025-06-30 20:01:06 +0200

    ``` Cleanup: Remove extra blank lines in tests.sh ```

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,4 @@
 
-
 # make functions for tests of key operations
 # such as cgi-param get/set etc, and cliuster conf, hostconf, restarts etc
 # also test cgi functions as follows
@@ -9,4 +8,6 @@
 env QUERY_STRING="mac=52540061c8c9&cmd=determine_state" bash -x /srv/hps-system/http/cgi-bin/boot_manager.sh
 
 # then run test suite from here
+
+
 
