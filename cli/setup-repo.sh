95cf54a64c29350de63660e2fe468c7b751341c5
SJM <sjm@opusvl.com>
2025-07-29 19:07:53 +0200

    Add source file retrieval for ZFS setup
    - Added a command to fetch and register the ZFS source file from the specified URL.

--- cli/setup-repo.sh
+++ cli/setup-repo.sh
@@ -13,3 +13,6 @@ OSVER_MAJ="10"
 
 prepare_custom_repo_for_distro "${CPU}-${MFR}-${OSNAME}-${OSVER}" "$@"
 
+# fetch_and_register_source_file "https://github.com/openzfs/zfs/releases/download/zfs-2.2.4/zfs-2.2.4.tar.gz" build_zfs_source
+
+
