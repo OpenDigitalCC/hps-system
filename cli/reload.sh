befc580d2e10b3de2015fc82340d1140746b77a4
SJM <sjm@opusvl.com>
2025-09-23 18:38:37 +0200

    ``` Comment out configure_supervisor_services call
    Temporarily disable the call to 'configure_supervisor_services'
    in reload.sh, while keeping the restart of all services active.
    ```

--- cli/reload.sh
+++ cli/reload.sh
@@ -6,6 +6,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/../lib/functions.sh"
 
 echo "Reloading $(get_active_cluster_filename)"
 
-configure_supervisor_services
+#configure_supervisor_services
 hps_services_restart  # Restart all services
       
