def4b1f4ebd33bbcb9ad5e53f9ffbc571a58ca4c
SJM <sjm@opusvl.com>
2025-09-29 17:12:17 +0200

    Add dnsmasq config generation with enhanced options
    Improved the create_config_dnsmasq function:
    - Configured interface binding to DHCP_IFACE
    - Refined DHCP and DNS options
    - Enabled TFTP support with proper sectioning
    - Consolidated comments for clarity and organization

--- lib/functions.d/create_config_dnsmasq.sh
+++ lib/functions.d/create_config_dnsmasq.sh
@@ -2,7 +2,6 @@ __guard_source || return
 # Define your functions below
 
 
-
 create_config_dnsmasq () {
 source $(get_active_cluster_filename 2>/dev/null)
 
@@ -13,36 +12,60 @@ if [[ -z "${DHCP_IP}" ]]; then
     exit 0
 fi
 
-echo "[*] Configuring dnsmasq on $DHCP_IP..." 
+hps_log info "Configuring dnsmasq on $DHCP_IP..." 
 
 cat > "${DNSMASQ_CONF}" <<EOF
 
 # dnsmasq base configuration for PXE/TFTP/DHCP
 
-# Do not use /etc/hosts or /etc/resolv.conf
-no-hosts
-no-resolv
+# ---------------------------
+# System config
+# ---------------------------
 
 # Bind only to this IP (set dynamically from cluster config)
 listen-address=${DHCP_IP}
 
 # Interface to bind to within the container
-interface=eth0 # Should this be ${DHCP_IFACE}?
+#interface=eth0 # Should this be ${DHCP_IFACE}?
+interface="${DHCP_IFACE}"
+
+# ---------------------------
+# DHCP configuration
+# ---------------------------
+
 
 # Enable DHCP
 dhcp-range=$(generate_dhcp_range_simple "$NETWORK_CIDR" "$DHCP_IP" 20)
+
+# Optional: Log DHCP requests
+log-dhcp
+
+# ---------------------------
+# DNS configuration
+# ---------------------------
+
+port=53
+domain="${DNS_DOMAIN}"
+dhcp-option=option:domain-search,"${DNS_DOMAIN}"
+#addn-hosts="${HPS_SERVICE_CONFIG_DIR}/dns_hosts"
+# DHCP reservations file
+dhcp-hostsfile="${HPS_SERVICE_CONFIG_DIR}/dns_hosts"
+dhcp-authoritative
+# Do not use /etc/hosts or /etc/resolv.conf
+no-hosts
+no-resolv
+
+# ---------------------------
+# TFTP
+# ---------------------------
 
 # Enable TFTP
 enable-tftp
 tftp-root=${HPS_TFTP_DIR}
 
-# Optional: Log DHCP requests
-log-dhcp
-
-# DNS configuration
-domain="${DNS_DOMAIN}"
-dhcp-option=option:domain-search,"${DNS_DOMAIN}"
-addn-hosts="${HPS_SERVICE_CONFIG_DIR}/dns_hosts"
+# ---------------------------
+# PXE config
+# ---------------------------
 
 # PXE-specific options (optional)
 #pxe-service=x86PC, "PXE Boot", pxelinux
