80a7a45343255cb5052aaac2150fd0dc0f582c2f
SJM <sjm@opusvl.com>
2025-10-17 19:15:24 +0200

    Add queue_actions.sh for Alpine network setup
    - Define actions to configure networking for Alpine.
    - Added `n_configure_syslog` to the action queue.

--- lib/node-functions.d/alpine.d/queue_actions.sh
+++ lib/node-functions.d/alpine.d/queue_actions.sh
@@ -15,11 +15,10 @@ n_queue_add n_configure_minimal_networking
 # Force network to started
 n_queue_add n_force_network_started
 
+
 # auto_load_network_modules
 #n_queue_add n_setup_network_modules_alpine
 n_queue_add n_auto_load_network_modules_safe
-
-
 
 
 n_queue_add n_storage_provision
@@ -36,5 +35,6 @@ n_queue_add n_force_start_services
 # OpenSVC
 n_queue_add n_install_apk_packages_from_ips opensvc-server opensvc-client
 
+n_queue_add n_configure_syslog
 
 
